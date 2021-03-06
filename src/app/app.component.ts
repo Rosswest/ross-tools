d3c57d9a8ee4d910a646fa084c7688e3984610c5
ASL <rosswest@hotmail.co.uk>
2021-03-06 20:13:26 +0000

    Added menu to main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'depictor';
+  menuItems: MenuItem[];
+
+  ngOnInit() {
+    this.menuItems = [
+      {label: 'Ising', icon: 'pi pi-fw pi-calendar'},
+      {label: 'SIRS', icon: 'pi pi-fw pi-pencil'},
+      {label: 'Transposer', icon: 'pi pi-fw pi-file'}
+    ];  }
+
 }
