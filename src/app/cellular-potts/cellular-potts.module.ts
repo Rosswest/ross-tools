a3de6f4faa867f91628dfe3133fc356e645a9190
Ross West <rosswest@hotmail.co.uk>
2021-06-12 18:05:26 +0100

    Revert "Revert "Fixed incorrect value on Kawasaki dynamics for Ising Model""
    This reverts commit 30b95fe222f1bf5d2b5885ce1e94604db5e69a46.

--- src/app/cellular-potts/cellular-potts.module.ts
+++ src/app/cellular-potts/cellular-potts.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CellularPottsComponent } from './cellular-potts.component';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FieldsetModule } from 'primeng/fieldset';
+import { DropdownModule } from 'primeng/dropdown';
 
 @NgModule({
   declarations: [CellularPottsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FieldsetModule,
+    DropdownModule
   ]
 })
 export class CellularPottsModule { }
--- src/app/potts/potts.module.ts
+++ src/app/potts/potts.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PottsComponent } from './potts.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FieldsetModule } from 'primeng/fieldset';
+import { DropdownModule } from 'primeng/dropdown';
 
 
 
 @NgModule({
   declarations: [PottsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FieldsetModule,
+    DropdownModule
   ]
 })
 export class PottsModule { }
